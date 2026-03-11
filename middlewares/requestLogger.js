f4e84461c911ca3f2d2b108c02a400deb40d27ce
Deeksha <sharmadeekksha@gmail.com>
2026-03-11 22:09:50 +0530

    feat(logging): add morgan request logger with file stream and error filtering

--- middlewares/requestLogger.js
+++ middlewares/requestLogger.js
@@ -2,13 +2,20 @@ const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 
+const logDirectory = path.join(__dirname, "../logs");
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory);
+}
+
 const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "../logs/access.log"),
+  path.join(logDirectory, "access.log"),
   { flags: "a" }
 );
 
 const requestLogger = morgan("combined", {
   stream: accessLogStream,
+  skip: (req, res) => res.statusCode < 400,
 });
 
 module.exports = requestLogger;
