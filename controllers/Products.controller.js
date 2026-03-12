efc1ce3aac62f2c1205eb577dd00d05e4b88f768
Deeksha <sharmadeekksha@gmail.com>
2026-03-12 15:41:18 +0530

    refactor(products): remove try-catch block

--- controllers/Products.controller.js
+++ controllers/Products.controller.js
@@ -3,25 +3,24 @@ const asyncHandler = require("../utils/asyncHandler");
 
 //   GET /api/products?filter
 const getAllProducts = asyncHandler(async (req, res) => {
-  try {
-    const filters = req.query;
+  const filters = req.query;
 
-    const query = {};
+  const query = {};
 
-    if (filters.category) {
-      query.category = filters.category;
-    }
+  if (filters.category) {
+    query.category = filters.category;
+  }
 
-    if (filters.subCategory) {
-      query.subCategory = filters.subCategory;
-    }
+  if (filters.subCategory) {
+    query.subCategory = filters.subCategory;
+  }
 
-    if (filters.discount) {
-      query.discount = {
-        ...{ $gte: 0 },
-        ...{ $lte: filters.discount },
-      };
-    }
+  if (filters.discount) {
+    query.discount = {
+      ...{ $gte: 0 },
+      ...{ $lte: filters.discount },
+    };
+  }
 
     if (
       (filters.minPrice !== undefined && filters.minPrice !== "") ||
@@ -35,38 +34,29 @@ const getAllProducts = asyncHandler(async (req, res) => {
       };
     }
 
-    if (filters.brand) {
-      query.brand = { $in: filters.brand.split(",") };
-    }
+  if (filters.brand) {
+    query.brand = { $in: filters.brand.split(",") };
+  }
 
-    const products = await Products.find(query);
+  const products = await Products.find(query).lean();
 
-    if (!products || products.length === 0) {
-      return res.status(200).json({
-        success: false,
-        message: "No products found matching the filters.",
-        data: [],
-      });
-    }
-    res.send({ success: true, data: products });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+  // check if removing below condition shall not break fe logic
+  if (!products || products.length === 0) {
+    return res.status(200).json({
+      success: false,
+      message: "No products found matching the filters.",
+      data: [],
+    });
   }
 });
 
 //  GET /api/products/product/:id
 const getSingleProduct = asyncHandler(async (req, res) => {
-  try {
-    const product = await Products.findOne({ productId: req.params.id });
-    if (!product)
-      return res
-        .status(404)
-        .json({ success: false, message: "Product Not Found!" });
-    res.send({ success: true, data: product });
-  } catch (error) {
-    console.log("Error fetching Product", error);
-  }
+
+  if (!product)
+    return res
+      .status(404)
+      .json({ success: false, message: "Product Not Found!" });
 });
 
 module.exports = {
