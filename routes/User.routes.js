449787677836cc51e473dfb3c0d463d4e4eb04b1
Deeksha <sharmadeekksha@gmail.com>
2026-03-12 19:19:58 +0530

    feat(routes): integrate zod validation in user routes

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -8,8 +8,20 @@ const {
   logoutUser,
 } = require("../controllers/User.controller");
 
-userRouter.route("/signup").post(signupUser);
-userRouter.route("/emailAuth").post(verifyEmail);
-userRouter.route("/passwordAuth").post(verifyPassword);
+const validateRequest = require("../middlewares/validateRequest");
+
+const {
+  signupSchema,
+  emailAuthSchema,
+  passwordAuthSchema,
+} = require("../validations/user.schema");
+
+userRouter.route("/signup").post(validateRequest(signupSchema), signupUser);
+userRouter
+  .route("/emailAuth")
+  .post(validateRequest(emailAuthSchema), verifyEmail);
+userRouter
+  .route("/passwordAuth")
+  .post(validateRequest(passwordAuthSchema), verifyPassword);
 userRouter.route("/logout").post(logoutUser);
 module.exports = userRouter;
