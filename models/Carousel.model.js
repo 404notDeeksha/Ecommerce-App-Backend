87324ad3a758dc2d30c6e2b09cb8b0590727b05c
Deeksha <sharmadeekksha@gmail.com>
2026-03-12 13:20:10 +0530

    refactor(carousel-schema): add trim to prevent accidental whitespaces

--- models/Carousel.model.js
+++ models/Carousel.model.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
+// update keys to camelCase in be & fe
 const carouselSchema = new mongoose.Schema(
   {
     category_id: {
       type: String,
       required: true,
       unique: true,
+      trim: true,
     },
     display_type: {
       type: String,
       required: true,
+      trim: true,
     },
     category_image_address: {
       type: String,
