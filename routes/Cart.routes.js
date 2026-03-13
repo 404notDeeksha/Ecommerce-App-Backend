8652a919a4edc8794ed557f02346af4e25944d7a
Deeksha <sharmadeekksha@gmail.com>
2026-03-13 15:33:57 +0530

    feat(cart): add Zod validation middleware for cart routes

--- routes/Cart.routes.js
+++ routes/Cart.routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cartController = require("../controllers/Cart.controller");
+const validateRequest = require("../middlewares/validateRequest");
+const {
+  addCartItemsSchema,
+  getCartSchema,
+  getCartQtySchema,
+  updateCartQtySchema,
+  deleteCartItemSchema,
+} = require("../validations/cart.schema");
 const router = express.Router();
 
-router.post("/", cartController.addCartItems);
-router.get("/:userId", cartController.getCart);
-router.get("/quantity/:userId", cartController.getCartQty);
-router.put("/:userId/:productId/:quantity", cartController.updateCartQty);
-router.delete("/:userId/:productId", cartController.deleteCartItem);
+router.post("/", validateRequest(addCartItemsSchema), cartController.addCartItems);
+router.get("/:userId", validateRequest(getCartSchema), cartController.getCart);
+router.get("/quantity/:userId", validateRequest(getCartQtySchema), cartController.getCartQty);
+router.put("/:userId/:productId/:quantity", validateRequest(updateCartQtySchema), cartController.updateCartQty);
+router.delete("/:userId/:productId", validateRequest(deleteCartItemSchema), cartController.deleteCartItem);
 
 module.exports = router;
