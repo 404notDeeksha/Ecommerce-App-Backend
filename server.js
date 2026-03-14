7be9185289c66431e9e151abc4e0545fd213fe08
Deeksha <sharmadeekksha@gmail.com>
2026-03-14 21:04:03 +0530

    style(log): remove redundant code

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const morgan = require("morgan");
 const router = require("./routes/index.routes");
 const env = require("./config/envValidator");
 const requestLogger = require("./middlewares/requestLogger");
