500c8fc3f6481ca261f09de133ee0dac1aac724b
Deeksha <sharmadeekksha@gmail.com>
2026-04-14 22:59:03 +0530

    style: rearrange routes as per usage by user

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const authRouter = require("./auth.routes");
 const userRouter = require("./User.routes");
+const CarouselRouter = require("./Carousel.routes");
 const ProductRouter = require("./Products.routes");
 const CartRouter = require("./Cart.routes");
-const CarouselRouter = require("./Carousel.routes");
-const authRouter = require("./auth.routes");
 
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
